7038ec168078b8fb4a6d0d399b992500c0d8f3c8
paulohssousa <paulohssousa@gmail.com>
2022-02-25 13:11:49 -0300

    Seção sobre 'Objeto' terminada

---
+++ JavaScript/Objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NAO EXECUTAVEL!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objecto = {
+    valor1,
+    valor2,
+    valor2,
+    
+    processamento() {
+        // ...
+    }
+}
+
+objecto.processamento() // Foco passou a ser objeto
+
+// Principios importantes
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo

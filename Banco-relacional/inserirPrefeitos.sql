9a0a8ae3b82d39523c7b694a4b73c2e3c6f28fb9
paulohssousa <paulohssousa@gmail.com>
2022-10-25 19:37:01 -0300

    Banco-relacional

---
+++ Banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,21 @@
+SELECT * FROM cidades
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 2),
+    ('Raquel Lyra', 3),
+    ('Zenaldo Coutinho', NULL);
+
+SELECT * FROM prefeitos;
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rafael Greca', NULL);
+
+-- ERRO!
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Pinheiro', 3);
